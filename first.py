8df19cc7fd708adfec0a8bb8592599af45daeba6
svmadap <suchirmadap@gmail.com>
2021-04-21 14:42:58 -0400

    recursive trial

--- first.py
+++ first.py
@@ -6,3 +6,11 @@ print('saketh')
 for i in range(7):
     print('jello')
 
+def recursive_prac(hello):
+    if hello > 1:
+        return hello + recursive_prac(hello - 1)
+    if hello == 1:
+        return hello 
+
+
+print(recursive_prac(3))
