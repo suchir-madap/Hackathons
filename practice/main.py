aebcf04c11c5b5179c366b9cd26f578ead185be2
skamuju <63597158+skamuju@users.noreply.github.com>
2021-05-08 00:12:06 -0400

    home and login routes

--- practice/main.py
+++ practice/main.py
@@ -3,3 +3,37 @@
 
 from flask import Flask, jsonify, request
 from pymongo import MongoClient, operations
+
+app = Flask(__name__)
+
+#create our routes
+
+@app.route('/home', methods=['POST'])
+def register():
+    data = request.get_json()
+    client = MongoClient('mongodb+srv://flimiso12:flimiso12@cluster0.hwuin.mongodb.net/FLI?retryWrites=true&w=majority')
+    db = client['FLI']
+    collection = db['users']
+
+    post = {'username': data['username'], 'password': data['password']}
+    collection.insert_one(post)
+    return jsonify({'success': True})
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    client = MongoClient('mongodb+srv://flimiso12:flimiso12@cluster0.hwuin.mongodb.net/FLI?retryWrites=true&w=majority')
+    db = client['FLI']
+    collection = db['users']
+
+
+    post = {'username': data['username'], 'password': data['password']}
+    if collection.find_one(post):
+        return jsonify({'success': True})
+    
+    else:
+        return jsonify({'success': False})
+
+
+
+app.run(host = '0.0.0.0')
