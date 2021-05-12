0553e74f22e861726bf3a80c2ad40e5eb410f92f
skamuju <63597158+skamuju@users.noreply.github.com>
2021-05-12 09:38:32 -0400

    Update

--- FLI/main.py
+++ FLI/main.py
@@ -12,31 +12,6 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    client = MongoClient('mongodb+srv://flimiso12:flimiso12@cluster0.hwuin.mongodb.net/FLI?retryWrites=true&w=majority')
-    db = client['FLI']
-    collection = db['users']
-
-    post = {"username": data["username"], "password": data["password"]}
-    collection.insert(post)
-    return jsonify({"success": True})
-
-@app.route('/login', methods=['GET'])
-def login():
-    data = request.get_json()
-    client = MongoClient('mongodb+srv://flimiso12:flimiso12@cluster0.hwuin.mongodb.net/FLI?retryWrites=true&w=majority')
-    db = client['FLI']
-    collection = db['users']
-
-    post = {"username": data["username"], "password": data["password"]}
-    if collection.find_one(post):
-        return jsonify({"success": True})
-    
-    else:
-        return jsonify({"success": False})
-
 @app.route('/events', methods = ['POST', 'GET'])
 def events():
     return 'events'
@@ -56,6 +31,30 @@ def join_us():
 @app.route('/contact_us', methods = ['POST', 'GET'])
 def contact_us():
     return 'contact_us'
+
+@app.route('/login', methods=['GET'])
+def login():
+    data = request.get_json()
+    client = MongoClient('mongodb+srv://flimiso12:flimiso12@cluster0.hwuin.mongodb.net/FLI?retryWrites=true&w=majority')
+    db = client['FLI']
+    collection = db['users']
+
+    def register_m():
+        post = {"username": data["username"], "password": data["password"]}
+        if collection.find_one(post):
+            return jsonify({"success": True})
+    
+        else:  
+            return jsonify({"success": False})
+        
+    
+    def login_m():
+        post = {"username": data["username"], "password": data["password"]}
+        if collection.find_one(post):
+            return jsonify({"success": True})
+        
+        else:
+            return jsonify({"success": False})
 
 
 
